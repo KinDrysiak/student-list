7df76740ffd12e0743772b94fa2be21bc301158b
KinDrysiak <kindrysiak@gmail.com>
2021-10-28 17:55:06 +0200

    Initial commit

---
+++ src/test/java/tomasz/jedrzejczyk/student/list/StudentListApplicationTests.java
@@ -0,0 +1,13 @@
+package tomasz.jedrzejczyk.student.list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
